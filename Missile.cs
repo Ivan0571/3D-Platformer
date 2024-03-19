ba69c3650d07b7b40b36b684ef3ef89aa9661e64
Ivan0571 <144049895+Ivan0571@users.noreply.github.com>
2024-03-19 21:35:34 +0500

    Add files via upload

---
+++ Missile.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward *speed * Time.deltaTime;
+        Destroy(gameObject,3);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Enemy enemy = other.GetComponent<Enemy>();
+        Destroy(enemy.gameObject);
+        Destroy(gameObject);
+    }
+}
